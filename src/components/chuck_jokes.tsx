6bf163aa63f525f0220cc4933ecfa76db58ad14d
Cenk <muzzyathome@gmail.com>
2022-04-06 15:13:29 +0100

    jokes added

--- src/components/chuck_jokes.tsx
+++ src/components/chuck_jokes.tsx
@@ -1,10 +1,11 @@
 // An alternative way of declaring a component is to write it as a function which
+
 // returns a JSX.Element 
-function ChuckJokes() : JSX.Element {
+function ChuckJokes(joke: string) : JSX.Element {
 
     return (
         <>
-            <p></p>
+            <p>{joke}</p>
         </>
     )
 }
